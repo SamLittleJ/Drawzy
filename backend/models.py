11bce735f9f4493cd323b04e8711e885e5636caa
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-03-31 16:27:52 +0300

    Created the table users for our database with SqlAlchemy

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+from datetime import datetime
+
+class user(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(128), nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
