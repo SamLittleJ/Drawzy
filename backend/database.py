58114aefaa9a00552786402ad881d03056aa92ce
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-04-30 18:29:56 +0300

    Added self inserting rds output and changed the order of the ci-cd workflow

--- backend/database.py
+++ backend/database.py
@@ -9,15 +9,9 @@ env_path = Path(__file__).parent / '.env'
 load_dotenv(env_path)
 
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT", "3306")
-print("DB_PORT: ", DB_PORT)
-
-#Get the database URL from the .env file
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set in .env file")
 
 #Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
