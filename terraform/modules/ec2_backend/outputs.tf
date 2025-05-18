24d371eba6997767d5f4e841fa50b59fcf05d922
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-05-18 11:43:52 +0300

    Cateva modificari in legatura cu structura terraform

--- terraform/modules/ec2_backend/outputs.tf
+++ terraform/modules/ec2_backend/outputs.tf
@@ -7,3 +7,8 @@ output "backend_tg_arn" {
   description = "The ARN of the backend target group"
   value = aws_lb_target_group.backend_tg.arn
 }
+
+output "backend_load_balancer_dns" {
+  description = "DNS of the load balancer"
+  value = aws_lb.backend_alb.dns_name
+}
--- terraform/modules/ec2_frontend/outputs.tf
+++ terraform/modules/ec2_frontend/outputs.tf
@@ -7,3 +7,8 @@ output "frontend_tg_arn" {
   description = "ARN of the frontend target group"
   value = aws_lb_target_group.frontend_tg.arn
 }
+
+output "frontend_load_balancer_dns" {
+  description = "DNS of the load balancer"
+  value = aws_lb.frontend_alb.dns_name
+}
