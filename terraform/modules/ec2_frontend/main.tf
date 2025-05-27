0419eeb3af7981b5b2e2be2c0cac89eb359e36d7
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-05-27 14:27:13 +0300

    Current IP for ssh connect

--- terraform/modules/ec2_frontend/main.tf
+++ terraform/modules/ec2_frontend/main.tf
@@ -181,7 +181,7 @@ resource "aws_security_group" "alb_sg_frontend" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["82.77.109.35/32"]
+    cidr_blocks = ["82.77.109.158/32"]
   }
 
   egress {
