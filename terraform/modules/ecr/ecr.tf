3ca3f19f9d31cad255087cbfc5c042464d91672c
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-05-28 21:58:29 +0300

    ECR change to not fill storage

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -6,6 +6,28 @@ resource "aws_ecr_repository" "backend" {
 #     prevent_destroy = true
 #   }
  }
+
+resource "aws_ecr_repository_policy" "backend_policy" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = <<EOF
+  {
+  "rules": [
+  {
+    "rulePriority": 1,
+    "description": "Pastreaza ultimele 10 imagini",
+    "selection": {
+      "tagStatus": "any",
+      "countType": "imageCountMoreThan",
+      "countNumber": 5
+      },
+    "action": {
+      "type": "expire"
+      }
+    }]}
+EOF
+}
+
 resource "aws_ecr_repository" "frontend" {
   name = "drawzy-frontend"
     image_tag_mutability = "MUTABLE"
@@ -13,6 +35,27 @@ resource "aws_ecr_repository" "frontend" {
   # lifecycle {
   #   prevent_destroy = true
   # }
+}
+
+resource "aws_ecr_repository_policy" "frontend_policy" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = <<EOF
+  {
+  "rules": [
+  {
+    "rulePriority": 1,
+    "description": "Pastreaza ultimele 10 imagini",
+    "selection": {
+      "tagStatus": "any",
+      "countType": "imageCountMoreThan",
+      "countNumber": 5
+      },
+    "action": {
+      "type": "expire"
+      }
+    }]}
+EOF
 }
 
 output "backend_ecr_url" {
