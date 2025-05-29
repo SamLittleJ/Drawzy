5fbf102209e691f13b179854472285e7ed54d33c
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-05-29 14:06:11 +0300

    Proper ip for ssh connect

--- terraform/modules/ec2_frontend/main.tf
+++ terraform/modules/ec2_frontend/main.tf
@@ -209,7 +209,7 @@ resource "aws_security_group" "ec2_sg_frontend" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["82.77.109.35/32"]
+    cidr_blocks = ["82.77.109.158/32"]
   }
 
   egress {
