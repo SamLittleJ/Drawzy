3a2826d3b68f6553cecba37019e2ff259d022dfc
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-05-29 16:45:36 +0300

    Main page change to homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import LobbyPage from './pages/LobbyPage';
@@ -14,12 +15,13 @@ export default function App() {
     return (
         <BrowserRouter>
         <Routes>
+            <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route 
             path="/lobby" 
             element={ isLoggedIn() ? <LobbyPage /> : <Navigate to="/login" replace />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         </BrowserRouter>
     );
