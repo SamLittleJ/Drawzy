c2e3c0439778d228ed0962aa2a0f4ef77b974d43
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-06-29 21:06:00 +0300

    Fixed the connections file

--- backend/routers/game_ws_manager.py
+++ backend/routers/game_ws_manager.py
@@ -7,39 +7,44 @@ class ConnectionManager:
         self.active_connections: dict[str, list[WebSocket]] = defaultdict(list)
         self.active_game_connections: dict[str, list[WebSocket]] = defaultdict(list)
         
-        async def connect(self, room_code: str, websocket: WebSocket):
-            await websocket.accept()
-            self.active_connections[room_code].append(websocket)
-            
-        def disconnect(self, room_code: str, websocket: WebSocket):
-            self.active_connections[room_code].remove(websocket)
-            
-        async def broadcast(self, room_code: str, message: dict):
-            data = json.dumps(message)
-            for ws in self.active_connections[room_code]:
-                try:
-                    await ws.send_text(data)
-                except:
-                    self.disconnect(room_code, ws)
-                    
-        async def connect_game(self, room_code: str, websocket: WebSocket):
-            await websocket.accept()
-            self.active_game_connections[room_code].append(websocket)
-            
-        def disconnect_game(self, room_code: str, websocket: WebSocket):
-            connections = self.active_game_connections.get(room_code, [])
-            if websocket in connections:
-                connections.remove(websocket)
-                if not connections:
-                    self.active_game_connections.pop(room_code, None)
-                    
-        async def broadcast_game(self, room_code: str, message: dict):
-            data = json.dumps(message)
-            for ws in list(self.active_game_connections.get(room_code, [])):
-                try:
-                    await ws.send_text(data)
-                except:
-                    self.disconnect_game(room_code, ws)
+
+    async def connect(self, room_code: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.setdefault(room_code, []).append(websocket)
+
+    def disconnect(self, room_code: str, websocket: WebSocket):
+        connections = self.active_connections.get(room_code, [])
+        if websocket in connections:
+            connections.remove(websocket)
+            if not connections:
+                self.active_connections.pop(room_code, None)
+
+    async def broadcast(self, room_code: str, message: dict):
+        data = json.dumps(message)
+        for ws in list(self.active_connections.get(room_code, [])):
+            try:
+                await ws.send_text(data)
+            except:
+                self.disconnect(room_code, ws)
+
+    async def connect_game(self, room_code: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_game_connections.setdefault(room_code, []).append(websocket)
+
+    def disconnect_game(self, room_code: str, websocket: WebSocket):
+        connections = self.active_game_connections.get(room_code, [])
+        if websocket in connections:
+            connections.remove(websocket)
+            if not connections:
+                self.active_game_connections.pop(room_code, None)
+
+    async def broadcast_game(self, room_code: str, message: dict):
+        data = json.dumps(message)
+        for ws in list(self.active_game_connections.get(room_code, [])):
+            try:
+                await ws.send_text(data)
+            except:
+                self.disconnect_game(room_code, ws)
                     
 manager = ConnectionManager()
             
