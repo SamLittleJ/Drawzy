a10ec7e8666510d78c18a31aaacdf002652bd16a
Ciuraru Samuel-Junior <ciuraru.s.samueljunior22@stud.rau.ro>
2025-06-30 11:30:00 +0300

    Some more modifying around the websocket

--- backend/routers/game_ws_manager.py
+++ backend/routers/game_ws_manager.py
@@ -5,7 +5,6 @@ from fastapi import WebSocket
 class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[str, list[WebSocket]] = defaultdict(list)
-        self.active_game_connections: dict[str, list[WebSocket]] = defaultdict(list)
         
 
     async def connect(self, room_code: str, websocket: WebSocket):
@@ -25,24 +24,5 @@ class ConnectionManager:
                 await ws.send_text(data)
             except:
                 self.disconnect(room_code, ws)
-
-    async def connect_game(self, room_code: str, websocket: WebSocket):
-        self.active_game_connections.setdefault(room_code, []).append(websocket)
-
-    def disconnect_game(self, room_code: str, websocket: WebSocket):
-        connections = self.active_game_connections.get(room_code, [])
-        if websocket in connections:
-            connections.remove(websocket)
-            if not connections:
-                self.active_game_connections.pop(room_code, None)
-
-    async def broadcast_game(self, room_code: str, message: dict):
-        data = json.dumps(message)
-        for ws in list(self.active_game_connections.get(room_code, [])):
-            try:
-                await ws.send_text(data)
-            except:
-                self.disconnect_game(room_code, ws)
                     
 manager = ConnectionManager()
-            
